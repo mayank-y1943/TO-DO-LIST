944dbb695a938c52939ff055a9e40a73a8d087b7
mayank-y1943 <mayankyada1943@gmail.com>
2025-04-13 15:53:52 +0530

    3rd commit

--- script.js
+++ script.js
@@ -1,40 +1,52 @@
+document.querySelector('.js-todo-input').addEventListener('keydown', (event)=>{
+    if(event.key==='Enter') addTodo();
+});
+
+document.querySelector('.js-dueDate').addEventListener('keydown', (event)=>{
+    if(event.key==='Enter') addTodo();
+});
+
+document.querySelector('.js-add-button').addEventListener('click', ()=>{
+    addTodo();
+});
+
 const todoListObject=[];
         
-        updateToDo();
+updateToDo();
 
-        function updateToDo(){
-            let html='';
-            todoListObject.forEach(function(value, index){
-                // let name=value.name;
-                // let dueDate=value.dueDate;
-                let {name, dueDate}=value;
-                html+=`<div>${name}</div>
-                       <div>${dueDate}</div>
-                       <button class="delete-button" onclick="
-                        todoListObject.splice(${index}, 1);
-                        updateToDo();
-                       ">Delete</button>`;
-            });
-            document.querySelector('.js-to-do-list').innerHTML=html;
-        }
-
-        function addTodo(){
-            let el1=document.querySelector('.js-todo');
-            let el2=document.querySelector('.js-dueDate');
-            let name=el1.value;
-            let dueDate=el2.value;
-            if(name===''||dueDate===''){
-                let msg='Invalid Input';
-                document.querySelector('.js-invalid').innerHTML=msg;
-            }
-            else{
-                todoListObject.push({
-                    'name': name,
-                    'dueDate': dueDate
-                });
-                el1.value='';
-                el2.value='';
+function updateToDo(){
+    let html='';
+    todoListObject.forEach(function(value, index){
+        // let name=value.name;
+        // let dueDate=value.dueDate;
+        let {name, dueDate}=value;
+        html+=`<div>${name}</div>
+                <div>${dueDate}</div>
+                <button class="delete-button" onclick="
+                todoListObject.splice(${index}, 1);
                 updateToDo();
-                document.querySelector('.js-invalid').innerHTML='';
-            }
-        }
+                ">Delete</button>`;
+    });
+    document.querySelector('.js-to-do-list').innerHTML=html;
+}
+
+function addTodo(){
+    let el1=document.querySelector('.js-todo-input');
+    let el2=document.querySelector('.js-dueDate');
+    let name=el1.value;
+    let dueDate=el2.value;
+    if(name===''||dueDate===''){
+        let msg='Invalid Input';
+        document.querySelector('.js-invalid').innerHTML=msg;
+    }
+    else{
+        todoListObject.push({
+            'name': name,
+            'dueDate': dueDate
+        });
+        el1.value='';
+        el2.value='';
+        updateToDo();
+        document.querySelector('.js-invalid').innerHTML='';
+    }
+}
